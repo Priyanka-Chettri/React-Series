43eead3f6805662d0984da2c349b90030c5d7334
Priyanka-Chettri <priyankachettri85@gmail.com>
2024-07-19 15:06:13 +0530

    Tried creating a component called chai in  basic react & vite react folder

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return (
+        <h3>Chai is ready in basic react</h3>
+    )
+}
+
+export default Chai;
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,9 @@
+import Chai from './chai.jsx';
 
 function App() {
 
   return (
-   <h1>Chai aur react with vite</h1>
+   <Chai/>
   )
 }
 
